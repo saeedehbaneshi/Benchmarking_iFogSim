d0457279bc72678b609715163ce35688551dc2f2
saeedehbaneshi <baneshisaeedeh@gmail.com>
2025-04-30 02:18:27 +0200

    detection organized results and perf avg 5

--- ObjectDetection/yolo5s_detection_method/run_multiple_yolo5s.sh
+++ ObjectDetection/yolo5s_detection_method/run_multiple_yolo5s.sh
@@ -4,12 +4,15 @@ set -e
 RESULT_DIR="../assets/results_yolo5s_method"
 mkdir -p "$RESULT_DIR"
 
+#Running with Torch
+#python yolov5/detect.py --weights ../assets/models/yolov5s.pt --source ../assets/motion_mask_output.mp4
+
 for run in {1..1}; do
   echo "=== YOLOv5-s run #$run ==="
-  perf stat -r 5 \
+  perf stat -r 1 \
     -e instructions,cycles \
     -o "$RESULT_DIR/perf_yolo5s_${run}.txt" \
-    -- python yolov5/detect.py --weights ../assets/models/yolov5s.pt --source ../assets/motion_mask_output.mp4
+    -- python yolo5s_detection.py
 
   python3 parse_perf_yolo5s.py \
     "$RESULT_DIR/perf_yolo5s_${run}.txt" \
